ca2ea0cf1b1fd6e7894b81451299d76b27afc2ef
Kay Yan <kay.yan@daocloud.io>
2015-09-11 16:16:01 +0800

    init

--- simple_python.py
+++ simple_python.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Hello World!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>"
+    html = "<div style='text-align: center'><h3>Hello World!</h3>" \
+           "<b>Hostname:</b> {hostname}</div>"
     return html.format(hostname=socket.gethostname())
 
 if __name__ == "__main__":
